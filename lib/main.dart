f98e35445cee9eaf6c4a046ee12b3ad9caaf1559
Karma <yausanadhikari@gmail.com>
2026-01-03 05:52:34 +0545

    feat: initialize Hive in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
   runApp(const BandhanApp());
 }
 
